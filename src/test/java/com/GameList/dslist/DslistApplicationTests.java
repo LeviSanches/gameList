6d9370edb5c47b26a442e6837adc866fc5c829b7
Levi Sanches <levi_sanches@outlook.com>
2023-07-11 18:48:02 -0300

    Project Created

---
+++ src/test/java/com/GameList/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GameList.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
